aabce222812bb6251ed20f0dc2a571cb14bf93e8
Eduardo2020 <82918930+Eduardo2020-code@users.noreply.github.com>
2026-09-16 14:57:37 -0500

    Actualiza sitio Mambe con prs

--- mambe-hamburgueseria-angular/src/app/app.routes.ts
+++ mambe-hamburgueseria-angular/src/app/app.routes.ts
@@ -5,7 +5,7 @@ export const routes: Routes = [
     path: '',
     loadComponent: () =>
       import('./pages/home/home.component').then((component) => component.HomeComponent),
-    title: 'Mambe Hamburguesería | Sabor, cultura y tradición'
+    title: 'Mambe Hamburguesería'
   },
   {
     path: '**',
